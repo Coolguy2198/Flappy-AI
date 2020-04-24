2b08c6cfc238f6e85c621ed39b56c61318b51197
Will Morrissey <39009053+WillMo2198@users.noreply.github.com>
2020-04-24 13:17:45 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import randint, random
 import numpy as np
 import sys
 import pygame
+import math
 
 sys.setrecursionlimit(100000)
 pygame.init()
@@ -12,10 +13,6 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('FlappyAI')
 back = pygame.image.load('background.png').convert_alpha()
 back1 = pygame.image.load('back.jpg').convert()
-whoosh = pygame.mixer.Sound('whoosh.wav')
-slap = pygame.mixer.Sound('slap.wav')
-up = pygame.mixer.Sound('level_up.wav')
-evolve = pygame.mixer.Sound('Absorb.wav')
 clock = pygame.time.Clock()
 gravity = 0
 dead = False
@@ -45,17 +42,17 @@ class Bird(pygame.sprite.Sprite):
         self.in3 = 0
         self.in4 = 0
         self.in6 = 0
-        self.inputs = [[], [], [], [], [], []]
+        self.in7 = 0
+        self.inputs = [[0, 0, 0, 0]]
         self.gen = 1
-        self.num_weights = len(self.inputs)
         self.curr_bird = 0
         self.fitness_score = 0
         self.weights2 = []
         self.new_population = []
         for i in range(self.num + 1):
-            self.new_population.append(np.random.uniform(low=-1.0, high=1.0, size=(6, 10)))
-        for i in range(self.num+1):
-            self.weights2.append(np.random.uniform(low=-1.0, high=1.0, size=(10, 2)))
+            self.new_population.append(np.random.uniform(low=-1.0, high=1.0, size=(4, 6)))
+        for i in range(self.num + 1):
+            self.weights2.append(np.random.uniform(low=-1.0, high=1.0, size=(6, 2)))
         self.fitness = []
         for i in range(self.num):
             self.fitness.append([])
@@ -106,14 +103,6 @@ class Bird(pygame.sprite.Sprite):
 
     def score(self):
         self.score1 += 1
-        pygame.mixer.Sound.play(up)
-        pygame.mixer.music.stop()
-        if self.score1 > self.max_score:
-            self.max_score = self.score1
-
-    @staticmethod
-    def s(x):
-        return np.tanh(x)
 
     @staticmethod
     def text_objects(text, font):
@@ -123,20 +112,17 @@ class Bird(pygame.sprite.Sprite):
     def restart(self):
         global dead, gravity, ang
         dead = False
-        pygame.mixer.Sound.play(slap)
-        pygame.mixer.music.stop()
         if self.curr_bird == self.num:
-            pygame.mixer.Sound.play(evolve)
-            pygame.mixer.music.stop()
             self.gen += 1
             self.find_parents()
             self.curr_bird = 0
             self.fitness = []
+            pipe.my_list = [randint(200, 450)]
+            self.crossover()
+            self.mutate()
             for i in range(self.num):
                 self.fitness.append([])
-            self.crossover()
-            pipe.my_list = [randint(250, 450)]
-        self.fitness[self.curr_bird] = [self.fitness_score, self.new_population[self.curr_bird], self.weights2]
+        self.fitness[self.curr_bird] = [self.fitness_score, self.new_population[self.curr_bird], self.weights2[self.curr_bird]]
         self.fitness_score = 0
         ang = 0
         gravity = 0
@@ -144,57 +130,50 @@ class Bird(pygame.sprite.Sprite):
         self.y = height/2-100
         self.x = 0
         pipe.x_pos = width - 20
-        pipe.y_pos = randint(250, (750-115)-250)
+        pipe.y_pos = randint(200, 450)
         pipe.x_change = 3
         self.curr_bird += 1
+        if self.score1 > self.max_score:
+            self.max_score = self.score1
         self.score1 = 0
         game_loop()
 
     def get_data(self):
         self.fitness_score += 1
-        self.in1 = pipe.x_pos
-        self.in3 = (pipe.my_list[self.score1] - 29) - self.y
-        self.in4 = (pipe.my_list[self.score1] - 119) - self.y
-        self.in6 = pipe.my_list[self.score1] - (150 / 2)
-        self.inputs[0] = self.in1
-        self.inputs[1] = self.velocity
-        self.inputs[2] = self.in3
-        self.inputs[3] = self.in4
-        self.inputs[4] = self.y
-        self.inputs[5] = self.in6
+        self.inputs[0][0] = pipe.x_pos
+        self.inputs[0][1] = self.velocity
+        self.inputs[0][2] = math.sqrt(self.x**2+(self.y-pipe.my_list[self.score1])**2)
+        self.inputs[0][3] = math.sqrt(self.x**2+(pipe.my_list[self.score1]-(150/2)-self.y)**2)
         self.inputs = np.asarray(self.inputs)
-        self.inputs = self.inputs / np.linalg.norm(self.inputs)
 
     def find_parents(self):
         largest1 = 0
         largest2 = 0
         wlargest1 = 0
-        wlargest2 = 0
         second1 = 0
         second2 = 0
         wsecond1 = 0
-        wsecond2 = 0
         large = []
+        var = 0
         for i in self.fitness:
             large.extend([[i[0], i[1], i[2]]])
         for x in range(len(large)):
             if large[x][0] > largest2:
                 largest1 = large[x][1]
+                wlargest1 = large[x][2]
                 largest2 = large[x][0]
-            if largest2 > large[x][0] > second2:
+                var = x
+            if large[x][0] > second2 and x != var:
                 second1 = large[x][1]
+                wsecond1 = large[x][2]
                 second2 = large[x][0]
-            if large[x][0] > wlargest2:
-                wlargest1 = large[x][2][0]
-                wlargest2 = large[x][0]
-            if wlargest2 > large[x][0] > wsecond2:
-                wsecond1 = large[x][2][0]
-                wsecond2 = large[x][0]
-        self.parents = [largest1, second1]
+        self.parents = [np.asarray(largest1), np.asarray(second1)]
         self.parents1 = [wlargest1, wsecond1]
 
     def crossover(self):
-        for i in range(self.num):
+        self.new_population = []
+        self.weights2 = []
+        for i in range(self.num+1):
             if i > self.num:
                 break
             child = []
@@ -203,60 +182,40 @@ class Bird(pygame.sprite.Sprite):
             parent2 = self.parents[1]
             wparent1 = self.parents1[0]
             wparent2 = self.parents1[1]
-            start = randint(0, len(parent1))
-            end = randint(0, len(parent1))
-            while start == end or end < start:
-                start = randint(0, len(parent1))
-                end = randint(0, len(parent1))
-            start2 = randint(0, len(wparent1))
-            end2 = randint(0, len(wparent1))
-            while start2 == end2 or end2 < start2 or start2 > end2:
-                start2 = randint(0, len(parent1))
-                end2 = randint(0, len(parent1))
+            x1 = randint(0, len(parent1))
             for x in range(len(parent1)):
                 child.append(0)
+            for x in range(0, x1):
+                child[x] = parent1[x]
+            for x in range(x1, len(parent1)):
+                child[x] = parent2[x]
+            x2 = randint(0, len(wparent1))
             for x in range(len(wparent1)):
                 child2.append(0)
-            for x in range(0, start):
-                child[x] = parent1[x]
-            for x in range(start, end):
-                child[x] = parent2[x]
-            for x in range(end, len(child)):
-                child[x] = parent1[x]
-            for x in range(0, start2):
+            for x in range(0, x2):
                 child2[x] = wparent1[x]
-            for x in range(start2, end2):
+            for x in range(x2, len(wparent1)):
                 child2[x] = wparent2[x]
-            for x in range(end2, len(child2)):
-                child2[x] = wparent1[x]
-            self.new_population[i] = child
-            self.weights2[i] = child2
+            self.new_population.append(np.asarray(child))
+            self.weights2.append(child2)
 
     def mutate(self):
-        mutation_rate = .2
-        mutations = 0
+        mutation_rate = .65
         for swapped in range(self.num):
             individual = randint(0, self.num)
-            x = 0
             if random() > mutation_rate:
-                x += 1
-                if x > len(self.new_population[individual]):
-                    x = 0
-                test = self.new_population[individual]
-                mutations += 1
-                swap_with = int(random() * len(test))
-                gene1 = self.new_population[individual][x]
-                gene2 = self.new_population[individual][swap_with]
-                self.new_population[individual][swap_with] = gene2
-                self.new_population[individual][x] = gene1
+                gene1 = np.random.uniform(low=-1.0, high=1.0, size=(6, ))
+                x1 = randint(0, len(self.new_population[0])-1)
+                self.new_population[individual][x1] = gene1
+                gene2 = np.random.uniform(low=-1.0, high=1.0, size=(2, ))
+                x2 = randint(0, len(self.weights2[0])-1)
+                self.weights2[individual][x2] = gene2
 
     def ff(self):
-        l1 = self.s(np.dot(self.inputs, self.new_population[self.curr_bird]))
-        l2 = self.s(np.dot(l1, self.weights2[self.curr_bird]))
-        if l2[0] > 0 and not dead:
+        l1 = np.tanh(np.dot(np.asarray(self.inputs), self.new_population[self.curr_bird]))
+        l2 = np.tanh(np.dot(l1[0], self.weights2[self.curr_bird]))
+        if l2[0] > l2[1] and not dead:
             global gravity
-            pygame.mixer.Sound.play(whoosh)
-            pygame.mixer.music.stop()
             gravity = 0
             self.jump()
 
@@ -282,7 +241,6 @@ class Bird(pygame.sprite.Sprite):
 
 
 bird = Bird(40, 30, 5, 0, height/2-100, 15)
-pygame.display.set_icon(bird.image)
 
 
 class Pipe(pygame.sprite.Sprite):
@@ -311,12 +269,11 @@ class Pipe(pygame.sprite.Sprite):
         self.x_pos -= self.x_change
 
 
-pipe = Pipe(3, width, randint(250, (750-115)-250))
+pipe = Pipe(3, width, randint(200, 450))
 
 
 def game_loop():
     while True:
-        bird.rand_state = randint(0, 1)
         screen.blit(back1, (0, 0))
         bird.drop()
         pipe.render()
@@ -326,20 +283,20 @@ def game_loop():
         bird.render()
         bird.check_collision(pipe.rect1)
         bird.check_collision(pipe.rect2)
+        screen.blit(back, (0, 0))
         bird.get_data()
         bird.ff()
-        screen.blit(back, (0, 0))
         bird.score_display(int(width/2), 0)
         bird.gen_display(0, int(height - 60))
         bird.curr_display(0, 0)
-        bird.highscore(width/2, height-60)
+        bird.highscore(width-200, height-60)
         if pipe.x_pos < -26:
             pipe.x_pos = width
-            pipe.y_pos = randint(250, (750-115)-250)
+            pipe.y_pos = randint(200, 450)
             bird.passed = False
             bird.score()
             if bird.score1 > len(pipe.my_list) - 1:
-                pipe.my_list.append(randint(250, (750-115)-250))
+                pipe.my_list.append(randint(200, 450))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -354,7 +311,7 @@ def game_loop():
             ang = 90
         elif ang < -90:
             ang = -90
-        clock.tick(50)
+        clock.tick(60)
         pygame.display.flip()
 
 
